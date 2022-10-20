59eda700f02eaf83fddf54f8564d9ca92d85caa3
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-20 17:45:46 +0200

    commented code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -54,6 +54,8 @@ const team = [
     }
 ];
 
+// function for generating URL 
+
 function generateURL(objectList) {
 
     for (let i = 0; i < objectList.length; i++) {
@@ -73,9 +75,13 @@ for (let i = 0; i < team.length; i++) {
 
     const profile = team[i];
 
+    // created column 
+
     const column = document.createElement("div");
 
     column.classList.add("col-4");
+
+    // created card
 
     const card = document.createElement("div");
 
@@ -83,21 +89,31 @@ for (let i = 0; i < team.length; i++) {
 
     card.classList.add("mb-5");
 
+    // created card body
+
     const cardBody = document.createElement("div");
 
     cardBody.classList.add("card-body");
+
+    // created image fluid
 
     const memberImage = document.createElement("img");
 
     memberImage.classList.add("img-fluid");
 
+    // created title for member name
+
     const memberName = document.createElement("h5");
 
     memberName.classList.add("text-center");
 
+    // created paragraph for member role
+
     const memberRole = document.createElement("p");
 
     memberRole.classList.add("text-center");
+
+    // append all elements nested
 
     row.appendChild(column);
 
@@ -110,6 +126,8 @@ for (let i = 0; i < team.length; i++) {
     cardBody.appendChild(memberName);
 
     cardBody.appendChild(memberRole);
+
+    // dynamically added name role and image
 
     memberName.innerHTML = profile['name'];
 
