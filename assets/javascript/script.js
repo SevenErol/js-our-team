538fa8f36b09bc66ea651cc6db9dfc0e728e4511
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-20 14:09:21 +0200

    milestone 0 complete

---
+++ assets/javascript/script.js
@@ -0,0 +1,60 @@
+
+const imagesList = [
+    "./assets/img/wayne-barnett-founder.ceo.jpg",
+    "./assets/img/angela-caroll-chief-editor.jpg",
+    "./assets/img/walter-gordon-office-manager.jpg",
+    "./assets/img/angela-lopez-social-media-manager.jpg",
+    "./assets/img/scott-estrada-developer.jpg",
+    "./assets/img/barbara-ramos-graphic-designer.jpg"
+];
+
+const team = [
+
+    {
+        name: "Wayne Barnett",
+
+        role: "Founder CEO",
+
+        image: "./assets/img/wayne-barnett-founder.ceo.jpg"
+    },
+
+    {
+        name: "Angela Caroll",
+
+        role: "Chief Editor",
+
+        image: "./assets/img/angela-caroll-chief-editor.jpg"  
+    },
+
+    {
+        name: "Angela Caroll",
+
+        role: "Chief Editor",
+
+        image: "./assets/img/walter-gordon-office-manager.jpg"
+    },
+
+    {
+        name: "Angela Caroll",
+
+        role: "Chief Editor",
+
+        image: "./assets/img/angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        name: "Angela Caroll",
+
+        role: "Chief Editor",
+
+        image: "./assets/img/scott-estrada-developer.jpg"
+    },
+
+    {
+        name: "Angela Caroll",
+
+        role: "Chief Editor",
+
+        image: "./assets/img/barbara-ramos-graphic-designer.jpg"
+    }
+];
